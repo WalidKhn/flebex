38e9c9305b09a6e462658116807d7eb54a8628ff
Walid <walid.kheniche@epitech.eu>
2025-06-27 14:19:34 +0200

    Flebex

---
+++ flebexx (2)/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "@/components/hero"
+import AboutScams from "@/components/about-scams"
+import SubscriptionPlans from "@/components/subscription-plans"
+import ExpertDigital from "@/components/expert-digital"
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Hero />
+      <AboutScams />
+      <SubscriptionPlans />
+      <ExpertDigital />
+    </main>
+  )
+}
